ed2c84162f08943ec2737156bd9dab8553035dd5
Nezerwa <eligrand2000@gmail.com>
2023-09-17 20:44:38 +0200

    Add final solution

--- exercise2.js
+++ exercise2.js
@@ -1,3 +1,7 @@
+// Create a function called myFetch that
+//  should work as a simple version of the native fetch() API. The function myFetch should use the XMLHttpRequest
+//  to make a GET Request and return a promise that resolves with the
+//  request’s response and rejects with an error if any.
 const myFetch = (url) => {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
