dd773f3ede1084a76e969a12bd068e252b4b8d10
Nezerwa <eligrand2000@gmail.com>
2023-09-19 13:09:31 +0200

    Fetch data from api

--- Jokes you've been 'awaiting' for ... promise/fetchJokes.js
+++ Jokes you've been 'awaiting' for ... promise/fetchJokes.js
@@ -1,4 +1,11 @@
 async function sayJoke(apiUrl, jokeId) {
-  const endPoint = await fetch(apiUrl)
+  const endPoint = await fetch(apiUrl);
+  const response = await endPoint.json();
+  return response;
 }
 
+sayJoke(
+  "https://my-json-server.typicode.com/IbrahimBagalwa/test-json-placeholder/jokes"
+)
+  .then((value) => console.log(value))
+  .catch("you have an error");
