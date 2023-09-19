84bda07958bcafaaa8d3e97c1dab9beb1eaa51af
Nezerwa <eligrand2000@gmail.com>
2023-09-19 15:37:39 +0200

    Error handle using catch block

--- Jokes you've been 'awaiting' for ... promise/fetchJokes.js
+++ Jokes you've been 'awaiting' for ... promise/fetchJokes.js
@@ -1,11 +1,20 @@
 async function sayJoke(apiUrl, jokeId) {
-  const endPoint = await fetch(apiUrl);
-  const response = await endPoint.json();
-  return response;
+  try {
+    const endPoint = await fetch(apiUrl);
+    const response = await endPoint.json();
+    const filtered = await response.filter((item) => item.id == jokeId);
+    if(filtered.length == 0){
+      throw new Error(`No jokes found id: ${jokeId}`);
+    }else if(!response.ok){
+      throw new Error(`No jokes at url: ${url}`);
+    }
+    return filtered;
+  }catch{
+
+  }
 }
 
 sayJoke(
-  "https://my-json-server.typicode.com/IbrahimBagalwa/test-json-placeholder/jokes"
-)
-  .then((value) => console.log(value))
-  .catch("you have an error");
+  "https://my-json-server.typicode.com/IbrahimBagalwa/test-json-placeholder/jokes",
+  144
+).then((res) => console.log(res));
